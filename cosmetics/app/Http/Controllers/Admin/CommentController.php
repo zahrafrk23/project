a535441503f682ec222c1135ff4d2c6b9dbfb2ce
kimiya <kimiyaaa_mr@yahoo.com>
2019-12-13 13:49:23 -0800

    comment controller

---
+++ cosmetics/app/Http/Controllers/Admin/CommentController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $comments = Comment::where('aproved', 1)->latest()->get();
+        return view('admin.Comment.index', compact('comments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comment $comment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Comment  $comment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comment $comment)
+    {
+        $comment->update(['aproved' => 1]);
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Comment $comment
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+        return back();
+    }
+    public function unsuccess()
+    {
+        $comments = Comment::where('aproved', 0)->latest()->paginate(15);
+        return view('admin.Comment.unsuccess', compact('comments'));
+    }
+}
