d298bb3a8b5d4ac61517568fabce18287b301e01
sara <sarahashemi75@yahoo.com>
2019-12-13 13:21:34 +0330

    category controller

---
+++ cosmetics/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use App\Http\Requests\CategoryRequest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $category = Category::all();
+        return view('admin.category.index', compact('category'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+ 
