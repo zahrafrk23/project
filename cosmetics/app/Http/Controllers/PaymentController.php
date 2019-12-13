7f0edec44e1366db75dcb7647fb1e58c0d061be7
sara <sarahashemi75@yahoo.com>
2019-12-13 13:16:28 +0330

    sara added a function in paymentcontroller

--- cosmetics/app/Http/Controllers/PaymentController.php
+++ cosmetics/app/Http/Controllers/PaymentController.php
@@ -15,5 +15,12 @@ class PaymentController extends Controller
         $payments = Payment::latest()->get();
         return view('admin.payment.index', compact('payments'));
     }
+	    public function show()
+    {
+        $payment = Payment::all()->where('id', \request()->id)->first();
+        $address = Adress::all()->where('id', $payment->address)->first();
+        $products= Product::all()->whereIn('id' , explode('-',$payment->products));
+        return view('admin.payment.show', compact('payment', 'address','products'));
+    }
 
 }
