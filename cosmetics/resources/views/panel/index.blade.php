ab504f9cb6834a647d3771a6b6beddce2023deeb
kimiya <kimiyaaa_mr@yahoo.com>
2019-12-13 11:37:52 -0800

    user pannel edit

--- cosmetics/resources/views/panel/index.blade.php
+++ cosmetics/resources/views/panel/index.blade.php
@@ -43,7 +43,11 @@
                             </section>
                             <section>
                                 <span class="alert alert-primary" style="float: right">نام شما : </span>
-                                <span class="alert alert-info" style="float: right;width: 90%">{{auth()->user()->name}}</span>
+                                <span class="alert alert-info" style="float: right;width: 90%">{{auth()->user()->name}}
+                                <button type="button" style="float:left;" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+ویرایش
+</button>
+                                </span>
                                 <span class="alert alert-primary" style="float: right">ایمیل  شما : </span>
                                 <span class="alert alert-info" style="float: right;width: 90%">{{auth()->user()->email}}</span>
                             </section>
@@ -58,5 +62,28 @@
             </div>
         </div>
     </div>
-
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+    <form action="{{route('user.panel.update',auth()->user()->id)}}" method="post">
+        @method('PUT')
+        @csrf
+    <input name="name" placeholder="نام جدید" />
+    <button class="btn btn-primary">ثبت</button>
+    </form>
+      </div>
+      <div class="modal-footer">
+        
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
