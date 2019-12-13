0ff211f151f669580a1cabdd554ff0f3db1e55b5
sara <sarahashemi75@yahoo.com>
2019-12-13 13:15:00 +0330

    paymentcontroller eddited

--- cosmetics/app/Http/Controllers/PaymentController.php
+++ cosmetics/app/Http/Controllers/PaymentController.php
@@ -1,42 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Adress;
-use App\Http\Requests\PaymentRequest;
 use App\Payment;
 use App\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Session;
+use App\Http\Controllers\Controller;
 
 class PaymentController extends Controller
 {
-
-    public function index(PaymentRequest $request)
+    public function index()
     {
-        $user_id = auth()->user()->id;
-        $address = $request->select;
-        $address = Adress::where('id', $address)->first();
-        $products = Session::get('cart-products', []);
-        $ids = array_keys($products);
-        $products = Product::whereIn('id', $ids)->get();
-        $mahsool = $request->products;
-        $price = 0;
-        foreach ($products as $product) {
-            $price += $product->price;
-        }
-        Payment::create([
-            'user_id' => $user_id,
-            'status' => false,
-            'products' => $mahsool,
-            'price' => $price,
-            'resnumber' => '000000',
-            'address' => $address->id,
-        ]);
-
-        alert()->success('پرداخت با موفقیت انجام شد','تبریک')->persistent('مرسی');
-        return redirect()->back();
+        $payments = Payment::latest()->get();
+        return view('admin.payment.index', compact('payments'));
     }
-
 
 }
