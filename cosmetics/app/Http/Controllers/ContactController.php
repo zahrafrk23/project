2d834b97f4d9ea4ec66af1fd32ecfca577d6d991
sara <sarahashemi75@yahoo.com>
2019-12-19 23:45:29 +0330

    contactcontroller create

---
+++ cosmetics/app/Http/Controllers/ContactController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Contact;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('admin.contact.index',compact('contacts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+}
